f0800d31141ddb411643b3b0c0254232056660d4
Lymeng <lymengchun@gmail.com>
2021-11-11 13:59:10 +0700

    start activity done

---
+++ app/src/main/java/com/example/pet_feeding/StartActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pet_feeding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pet_feeding_start)
+
+
+    }
+}
