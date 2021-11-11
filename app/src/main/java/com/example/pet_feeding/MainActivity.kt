d6dba94eaa8e0fcb68edceeaa019bb760ea2c0f1
leang69 <ngounmengleang18@gmail.com>
2021-11-11 14:10:25 +0700

    config app theme color

--- app/src/main/java/com/example/pet_feeding/MainActivity.kt
+++ app/src/main/java/com/example/pet_feeding/MainActivity.kt
@@ -1,35 +1,59 @@
 package com.example.pet_feeding
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.pet_feeding.databinding.ActivityMainBinding
+import androidx.fragment.app.Fragment
+import com.example.pet_feeding.fragment.HomeFragment
+import com.example.pet_feeding.fragment.ProfileFragment
+import com.example.pet_feeding.fragment.SettingFragment
+import com.example.pet_feeding.fragment.schedule_feednow
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val homeFragment = HomeFragment()
+    private val profileFragment = ProfileFragment()
+    private val settingFragment = SettingFragment()
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        replaceFragment(profileFragment)
+
+
+        val bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.navigation_home -> replaceFragment(homeFragment)
+                R.id.navigation_profile -> replaceFragment(profileFragment)
+                R.id.navigation_setting -> replaceFragment(settingFragment)
+            }
+            true
+        }
+
+
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container,fragment)
+            commit()
+        }
+
+
+
     }
 }
+
+
---
+++ app/src/main/java/com/example/pet_feeding/fragment/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.pet_feeding.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.commit
+import com.example.pet_feeding.R
+
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bt_add_Device = view.findViewById<Button>(R.id.bt_add_device)
+
+        bt_add_Device.setOnClickListener {
+            replaceFragment(schedule_feednow())
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        requireActivity().supportFragmentManager.commit{
+            replace(R.id.fragment_container,fragment)
+        }
+
+        }
+
+}
